f837e7a87f48a641ce6e53282ce379ea4fcdfdf7
Aadam <aadimator@gmail.com>
2018-01-02 13:41:11 +0500

    feat: Update Location Model

--- Assets/Scripts/Models/Location.cs
+++ Assets/Scripts/Models/Location.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Location : MonoBehaviour {
+public class Location {
 
-	// Use this for initialization
-	void Start () {
-		
+	public long time;
+	public float speed;
+	public double altitude;
+	public double longitude;
+	public double latitude;
+
+	public Location(long time, float speed, double altitude, double longitude, double latitude) {
+		this.time = time;
+		this.speed = speed;
+		this.altitude = altitude;
+		this.longitude = longitude;
+		this.latitude = latitude;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
