73506a331b6cc705cdaaa61ac01d700f7d0f3771
Aadam <aadimator@gmail.com>
2018-03-01 15:49:52 +0500

    feat: Use Singleton pattern in DatabaseManager.cs

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 
 public class DatabaseManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public static DatabaseManager sharedInstance = null;
+
+	/// <summary>
+	/// Awake this instance and initialize sharedInstance for Singelton pattern
+	/// </summary>
+	void Awake() {
+		if (sharedInstance == null) {
+			sharedInstance = this;
+		} else if (sharedInstance != null) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
