734b0607b6d92e020d2c0175c4c4317865928683
Aadam <aadimator@gmail.com>
2018-04-02 12:45:44 +0500

    feat: SignOut implemented

---
+++ Assets/Scripts/Managers/AccountManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AccountManager : MonoBehaviour {
+
+    AuthManager authManager = AuthManager.sharedInstance;
+
+    public void OnSignOut()
+    {
+        Mapbox.Unity.Utilities.Console.Instance.Log("OnSignOut()", "lightblue");
+
+        if(authManager == null)
+        {
+            Debug.Log("authmanager is null");
+        }
+
+        authManager.SignOut();
+
+        Mapbox.Unity.Utilities.Console.Instance.Log("after auth.SignOut()", "lightblue");
+
+        SceneManager.LoadScene("Onboarding");
+
+        Mapbox.Unity.Utilities.Console.Instance.Log("after SceneChange", "lightblue");
+    }
+}
--- Assets/Scripts/Managers/AuthManager.cs
+++ Assets/Scripts/Managers/AuthManager.cs
@@ -6,74 +6,94 @@ using Firebase;
 using Firebase.Auth;
 using UnityEngine.SceneManagement;
 
-public class AuthManager : MonoBehaviour {
+public class AuthManager : MonoBehaviour
+{
 
-	// Firebase API variables
-	FirebaseAuth auth;
+    // Firebase API variables
+    FirebaseAuth auth;
 
-	public static AuthManager sharedInstance = null;
+    public static AuthManager sharedInstance = null;
 
-	// Delegates
-	public delegate IEnumerator AuthCallback ( Task<Firebase.Auth.FirebaseUser> Task, string operation);
-	public event AuthCallback authCallback;
+    // Delegates
+    public delegate IEnumerator AuthCallback(Task<Firebase.Auth.FirebaseUser> Task, string operation);
+    public event AuthCallback authCallback;
 
-	void Awake() {
-		if (sharedInstance == null) {
-			sharedInstance = this;
-		} else if (sharedInstance != this) {
-			Destroy (gameObject);
-		}
-
-		DontDestroyOnLoad (gameObject);
-
-		InitializeFirebase ();
-	}
-
-	public void SignUpNewUser(string email, string password) {
-		auth.CreateUserWithEmailAndPasswordAsync (email, password).ContinueWith (task => {
-			StartCoroutine(authCallback(task, "sign_up"));
-		});
-	}
-
-	public void LoginExistingUser(string email, string password) {
-		auth.SignInWithEmailAndPasswordAsync (email, password).ContinueWith (task => {
-			StartCoroutine(authCallback(task, "login"));
-		});
-	}
-
-    public void OnSignOut()
+    void Awake()
     {
-        auth.SignOut();
-        SceneManager.LoadScene("Onboarding");
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+        else if (sharedInstance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        InitializeFirebase();
     }
 
-	// Handle initialization of the necessary firebase modules:
-	void InitializeFirebase() {
-		Debug.Log("Setting up Firebase Auth");
-		auth = FirebaseAuth.DefaultInstance;
-		auth.StateChanged += AuthStateChanged;
+    public void SignUpNewUser(string email, string password)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            StartCoroutine(authCallback(task, "sign_up"));
+        });
+    }
 
-		AuthStateChanged(this, null);
-	}
+    public void LoginExistingUser(string email, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            StartCoroutine(authCallback(task, "login"));
+        });
+    }
 
-	void OnDestroy() {
-		auth.StateChanged -= AuthStateChanged;
-		auth = null;
-	}
+    public void SignOut()
+    {
+        //Mapbox.Unity.Utilities.Console.Instance.Log("Signing out", "lightblue");
 
-	// Track state changes of the auth object.
-	void AuthStateChanged(object sender, System.EventArgs eventArgs) {
-		Firebase.Auth.FirebaseAuth senderAuth = sender as Firebase.Auth.FirebaseAuth;
-		Firebase.Auth.FirebaseUser user = null;
-		if (senderAuth == auth && senderAuth.CurrentUser != user) {
-			bool signedIn = user != senderAuth.CurrentUser && senderAuth.CurrentUser != null;
-			if (!signedIn && user != null) {
-				Debug.Log("Signed out " + user.UserId);
-			}
-			user = senderAuth.CurrentUser;
-			if (signedIn) {
-				Debug.Log("Signed in " + user.UserId);
-			}
-		}
-	}
+        if (auth == null) Debug.Log("auth is null");
+
+        auth.SignOut();
+
+        //Mapbox.Unity.Utilities.Console.Instance.Log("Signing out after", "lightblue");
+    }
+
+    // Handle initialization of the necessary firebase modules:
+    void InitializeFirebase()
+    {
+        //Mapbox.Unity.Utilities.Console.Instance.Log("Setting up Firebase Auth", "lightblue");
+        auth = FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+
+        AuthStateChanged(this, null);
+    }
+
+    void OnDestroy()
+    {
+        auth.StateChanged -= AuthStateChanged;
+        auth = null;
+    }
+
+    // Track state changes of the auth object.
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        Firebase.Auth.FirebaseAuth senderAuth = sender as Firebase.Auth.FirebaseAuth;
+        Firebase.Auth.FirebaseUser user = null;
+        if (senderAuth == auth && senderAuth.CurrentUser != user)
+        {
+            bool signedIn = user != senderAuth.CurrentUser && senderAuth.CurrentUser != null;
+            if (!signedIn && user != null)
+            {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = senderAuth.CurrentUser;
+            if (signedIn)
+            {
+                Debug.Log("Signed in " + user.UserId);
+            }
+        }
+    }
 }
